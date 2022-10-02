4471d5957a481c3b3f95defa33aea29c510ef8fd
imgsing <43990200+imgsing@users.noreply.github.com>
2022-10-02 14:16:00 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+<Connector port="8080" protocol="HTTP/1.1" 
+           connectionTimeout="20000" 
+           redirectPort="8443" URIEncoding="UTF-8"/>
+catalina.sh/catalina.bat - added following
+
+set JAVA_OPTS=--Xms256m -Xmx1024m -Xss268k -server -XX:MaxPermSize=256m -XX:-UseGCOverheadLimit -Djava.awt.headless=true -Djavax.servlet.request.encoding=UTF-8 -Dfile.encoding=UTF-8
+
+set CATALINA_OPTS=-Dfile.encoding="UTF-8"
